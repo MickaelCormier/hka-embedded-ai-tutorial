5b4a0674d4b56b114e33ae4d227b26f6d6d5aec8
Mickael Cormier <mickael.cormier@iosb.fraunhofer.de>
2025-02-19 15:30:18 +0100

    Solutions Gstreamer

--- gstreamer/cpp/lib/Cpp_Gstreamer.h
+++ gstreamer/cpp/lib/Cpp_Gstreamer.h
@@ -5,11 +5,19 @@
 
 class Cpp_Gstreamer
 {
-private:
-
 public:
-    Cpp_Gstreamer();
+    Cpp_Gstreamer(const std::string a_video_path);
     ~Cpp_Gstreamer();
+
+    void run();
+
+private:
+    cv::VideoCapture m_cap;
+    double m_max_fps, m_min_fps, m_avg_fps;
+    std::vector<double> m_fps_list;
+
+    bool process_frame(cv::Mat& a_frame);
+    void update_fps_stats(double a_fps);
 };
 
 #endif // LIB_CPP_GSTREAMER_H_
--- gstreamer/py/py_gstreamer.py
+++ gstreamer/py/py_gstreamer.py
@@ -7,12 +7,58 @@ which calculates and outputs statistics about the process
 and also displays the images on the screen.
 """
 def main(a_video_path):
-    cap = cv2.VideoCapture("", cv2.CAP_GSTREAMER)
+    cap = cv2.VideoCapture(a_video_path)
+
+    # With resize from GStreamer.
+    # cap = cv2.VideoCapture(f'filesrc location={a_video_path} ! qtdemux ! queue ! h264parse ! nvv4l2decoder ! nvvidconv ! video/x-raw,format=BGRx,width=1280,height=720 ! queue ! videoconvert ! queue ! video/x-raw, format=BGR ! appsink', cv2.CAP_GSTREAMER)
+
+    # Without resizing.
+    # cap = cv2.VideoCapture(f'filesrc location={a_video_path} ! qtdemux ! h264parse ! nvv4l2decoder ! nvvidconv ! video/x-raw, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink', cv2.CAP_GSTREAMER)
+
 
     if not cap.isOpened():
         print("Error: Could not open video.")
 
 
+    frame_count = 0
+    total_time = 0
+    fps_list = []
+    max_fps = 0
+    min_fps = float('inf')
+
+    while True:
+        start_time = time.time()
+
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        frame_count += 1
+
+        cv2.imshow('Video', frame)
+
+        end_time = time.time()
+        frame_time = end_time - start_time
+        total_time += frame_time
+        fps = 1 / frame_time if frame_time > 0 else 0
+        fps_list.append(fps)
+
+        if fps > max_fps:
+            max_fps = fps
+        if fps < min_fps:
+            min_fps = fps
+
+        avg_fps = sum(fps_list) / len(fps_list) if fps_list else 0
+
+        print(f'Current FPS: {fps:.2f}, AVG FPS: {avg_fps:.2f}, Max FPS: {max_fps:.2f}, Min FPS: {min_fps:.2f}')
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
 if __name__ == "__main__":
-    video_path = ""
+    video_path = "traffic.mp4"
     main(video_path)
